b7155160f906462ef75d65769f2868eb86a8ea23
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-07-01 01:50:21 +0300

    зробив деякі зміни

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Menu from "./Components/Menu/menu"
 import Users from "./Components/Users/Users";
 export  default function App() {
   return(
       <div>
         <Users/>
+          <Menu/>
       </div>
   )
 }
