85fd8cbd757cfb0502901925750909d4abbfb734
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-07-06 21:16:45 +0300

    homework 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom"
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+import Comments from "./components/comments/Comments";
 
 
 
 function App() {
   return (
-    <div>
-
-    hello octen
-    </div>
+   <Router>
+     <div>
+       <div><Link to={'/'}>Home</Link></div>
+       <div><Link to={'/users'}>Users</Link></div>
+       <div><Link to={'/posts'}>Posts</Link></div>
+       <div><Link to={'/comments'}>Comments</Link></div>
+     </div>
+        <Switch>
+            <Route exact path={'/'} render ={()=> {
+                return <h1>Home</h1>
+            }} />
+            <Route path={'/users'}>
+                <Users/>
+            </Route>
+            <Route path={'/posts'}>
+                <Posts/>
+            </Route>
+            <Route path={'/comments'}>
+                <Comments/>
+            </Route>
+        </Switch>
+   </Router>
   );
 }
 
---
+++ src/services/Api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+let axiosInstance = axios.create({
+ baseURL:'https://jsonplaceholder.typicode.com'
+}
+)
+const getUsers = () => axiosInstance('/users');
+const getUser = (id) => axiosInstance('/users/' + id);
+const getPosts = () =>axiosInstance('/posts')
+const getUserPost = (id) =>axiosInstance('/users/' + id + '/posts')
+const getCommets = () => axiosInstance('/comments')
+const getCommentPost = (id) => axiosInstance('posts'+ id + '/comments' )
+export {getUsers,getUser,getPosts,getUserPost,getCommets,getCommentPost};
