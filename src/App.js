7a4af83b36cb581ccf3af3db4868f9a01213360d
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-07-06 17:36:43 +0300

    lection 3 part 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import {useReducer} from "react";
 
-
-
-
-function App() {
-  return (
-    <div>
-
-    hello octen
-    </div>
-  );
+function reducer(state, action) {
+    switch (action.type) {
+        case'Inc A':
+            return{...state, a: state.a + action.payload};
+        default:
+            return {...state}
+    }
 }
 
-export default App;
+
+export default function App() {
+    let [state, dispatch]=useReducer(reducer,{a:0, b:0})
+        return(
+            <div>
+              <button onClick={()=>dispatch({type:'Inc A', payload:1})}> click me A {state.a} -</button>
+              <button> inc B {state.b} </button>
+            </div>
+            )
+  }
