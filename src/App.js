ecb4460a3c17ac51d41aaa56caeaebf32aa6e7c1
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-08-06 15:03:46 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
-
+import {createRef} from "react";
 
 
 function App() {
+  let myFormRef = createRef()
+  let myButton = createRef()
+
+  const onForSubmit = (e) =>{
+    e.preventDefault()
+    console.log(myFormRef.current.username.value);
+    console.log(myButton.current);
+
+  }
   return (
     <div>
+      <form action={'/someUrl'} method={'post'}  onSubmit={onForSubmit} ref={myFormRef}>
+        <input type="text" name={'username'}/>
+        <button ref={myButton}>save</button>
 
-    hello octen
+      </form>
     </div>
   );
 }
